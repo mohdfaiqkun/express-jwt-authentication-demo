4cc14dfb1b7fc1594c3e2dfd4f28744d03d45972
mohdfaiqkun <83447337+mohdfaiqkun@users.noreply.github.com>
2021-08-11 17:09:09 +0800

    Changed code in in_memory_mongodb_setup to be compatible with the latest version of mongodb-memory-server

---
+++ src/model/user.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const { secret } = require("../../config/jwt");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    index: true,
+    unique: true,
+    lowercase: true,
+    required: [true, "cannot be blank"]
+  },
+  email: {
+    type: String,
+    index: true,
+    unique: true,
+    lowercase: true,
+    required: [true, "cannot be blank"]
+  },
+  password: {
+    passwordHash: String,
+    passwordSalt: String
+  }
+});
+
+UserSchema.methods.setPassword = function(password) {
+  this.passwordSalt = generateSalt();
+  this.passwordHash = hashPassword(password, this.passwordSalt);
+};
+
+function generateSalt() {
+  return crypto.randomBytes(16).toString("hex");
+}
+
+function hashPassword(password, salt) {
+  return crypto
+    .pbkdf2Sync(password, salt, 10000, 512, "sha512")
+    .toString("hex");
+}
+
+UserSchema.methods.validPassword = function(password) {
+  return this.passwordHash === hashPassword(password, this.passwordSalt);
+};
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      userid: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000)
+    },
+    secret
+  );
+};
+
+UserSchema.methods.verifyJWT = function(token) {
+  try {
+    jwt.verify(token, secret);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+module.exports = mongoose.model("User", UserSchema);
+UserSchema.plugin(uniqueValidator, { message: "should be unique" });
